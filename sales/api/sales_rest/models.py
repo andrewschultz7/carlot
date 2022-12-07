8092c08665f72e8ab71824f7efeeb8c0fe9c9e8a
Corey <clyon324@gmail.com>
2022-12-07 11:26:39 -0700

    created sales models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -8,16 +8,30 @@ class AutomobileVO(models.Model):
     sold = models.BooleanField()
 
 class SalesPersonVO(models.Model):
-    pass
-class CustomerVO(models.Model):
-    pass
-
-
-class SalesPerson(models.Model):
+    import_href = models.CharField(max_length=200, unique=True, null=True)
     name = models.CharField(max_length=200)
     employee_number = models.CharField(max_length=200)
 
-class Customer(models.Model):
+class CustomerVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True, null=True)
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
+
+class Sale(models.Model):
+    price = models.PositiveBigIntegerField()
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name = "sales",
+        on_delete = models.PROTECT,
+    )
+    sales_person = models.ForeignKey(
+        SalesPersonVO,
+        related_name = "sales",
+        on_delete=models.PROTECT,
+    )
+    customer = models.ForeignKey(
+        CustomerVO,
+        related_name="sales",
+        on_delete=models.PROTECT,
+    )
